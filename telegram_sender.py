a11b56263dd67553a13e2949a98c1e97d7f221bc
John <mail4tck@gmail.com>
2021-02-05 18:44:26 +0100

    space test

--- telegram_sender.py
+++ telegram_sender.py
@@ -9,4 +9,4 @@ def send(chat_id, message):
     token = os.getenv("TELEGRAM_TOKEN")
     url_req = f"{URL}{token}/sendMessage"
     r = requests.post(url_req, data={'chat_id': chat_id, "text": message})
-    print("Post text:", r.text)
+    print("Post text: ", r.text)
